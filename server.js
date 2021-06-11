bb8bc7319af670eca110ced0bfa9916ce459256d
clong desktop <chip_long@yahoo.com>
2021-06-11 01:42:51 -0600

    added homepage display

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(session(sess));
 
 // setup express to us json and routes
 app.use(express.json());
-app.use(express.urlencoded);
+app.use(express.urlencoded());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(routes);
 
