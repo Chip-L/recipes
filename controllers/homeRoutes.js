f22acc80929a2fb0761b8d98925e4227c9aee9b1
clong desktop <chip_long@yahoo.com>
2021-06-20 17:36:04 -0600

    added in normalize css and tweaked homeRoutes.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,12 +1,4 @@
 const router = require("express").Router();
-const {
-  User,
-  Ingredient,
-  Recipe,
-  RecipeIngredient,
-  RecipeSteps,
-} = require("../models");
-const { printItem, displayValues } = require("../utils/routeHelpers");
 
 /** initial get route for the homepage - this will return latest recipes added to the DB. */
 router.get("/", async (req, res) => {
