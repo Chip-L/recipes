4e09f1ffcea9b33a6e8018cb87d7922637955ce3
clong desktop <chip_long@yahoo.com>
2021-06-20 17:35:06 -0600

    separated test routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const homeRoutes = require("./homeRoutes");
+const testRoutes = require("./testRoutes");
 
 // set up the paths for the routes
 router.use("/", homeRoutes);
+router.use("/test", testRoutes);
 
 // export the router
 module.exports = router;
